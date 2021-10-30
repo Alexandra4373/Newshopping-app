0792fd46d1db9dd8a7d9b46d7c5e830ff8fdcc33
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2021-10-30 06:48:02 +0000

    new changes

---
+++ screens/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text ,Image } from "react-native";
+
+ export default function Login(){
+     return <View>
+         <Image source={{url="https://images.unsplash.com/photo-1571068316344-75bc76f77890?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"}} />
+     </View> 
+ }
